8e335eb9d526b28420386c15388d6c421ce6a875
Marcin <killavus@gmail.com>
2016-01-28 20:16:38 +0100

    KMeans to separate method

--- classifier.py
+++ classifier.py
@@ -27,20 +27,23 @@ validation_labels = validation.data_sources[1].T.ravel()
 test_data = np.reshape(test.data_sources[0], (10000, 3072))
 test_labels = test.data_sources[1].T.ravel()
 
-naive_grouping = KMeans(n_clusters=10, n_init=5, n_jobs=8)
-naive_grouping.fit(train_data)
 
-group_labels = naive_grouping.labels_
+def PerformKMeansAnalysis(train_data, train_labels, test_data, test_labels):
+    naive_grouping = KMeans(n_clusters=10, n_init=5, n_jobs=8)
+    naive_grouping.fit(train_data)
+    group_labels = naive_grouping.labels_
 
-for label in set(group_labels):
-    train_mask = group_labels == label
-    real_labels = train_labels[train_mask]
-    pos_stat = Counter(real_labels).values()
-    pos_stat = np.array(pos_stat).astype(np.float)
-    pos_stat /= real_labels.shape[0]
+    for label in set(group_labels):
+        train_mask = group_labels == label
+        real_labels = train_labels[train_mask]
+        pos_stat = Counter(real_labels).values()
+        pos_stat = np.array(pos_stat).astype(np.float)
+        pos_stat /= real_labels.shape[0]
 
-    representative = mode(real_labels)[0]
-    gini_index = np.dot(pos_stat.T, np.ones(pos_stat.shape[0]) - pos_stat)    
-    print "For label %d: %s" % (label, gini_index)
-    print "Representative: ", representative
-    print "======"
+        representative = mode(real_labels)[0]
+        gini_index = np.dot(pos_stat.T, np.ones(pos_stat.shape[0]) - pos_stat)
+        print "For label %d: %s" % (label, gini_index)
+        print "Representative: ", representative
+        print "======"
+
+

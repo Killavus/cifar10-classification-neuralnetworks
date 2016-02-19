b5900fac8e33386cacbd0d48390bda6e54903199
Marcin Grzywaczewski <killavus@gmail.com>
2016-02-19 04:29:22 +0100

    Removed inlined libraries (installed with pip --user now), modified fuel data path

--- set-env.sh
+++ set-env.sh
@@ -1,5 +1,5 @@
 PWD=$(pwd)
 
-export FUEL_DATA_PATH=$PWD/datasets
+export FUEL_DATA_PATH=/pio/scratch/1/i247948/
 export PYTHONPATH=$PWD/libs/Lasagne:$PWD/libs/Theano:$PWD/libs/fuel:$PWD/libs/picklable-itertools:$PWD/libs/progressbar2:$PYTHONPATH
-export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32,nvcc.fastmath=True
+export THEANO_FLAGS=device=gpu
